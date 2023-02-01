310aafbc91e48f0b62ac040da6090f4979f92fc4
orucmu <mustafaoruc720@gmail.com>
2023-02-01 22:16:18 +0300

    Effect Hooks - useEffect()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import NoteList from './components/NoteList'
+import { useState, useEffect } from "react";
 
 function App(props) {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState(props.text);
+
+  useEffect(() => {
+    console.log("count");
+  }, [count])
+
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, [])
+
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
   return (
     <>
-      <NoteList />
+      <p>Butona {count} kez tıkladınız.</p>
+      <p>Girilen text: {text}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
     </>
-  )
+  );
+}
+
+App.defaultProps = {
+  count: 5, text: ''
 }
 
 export default App;
