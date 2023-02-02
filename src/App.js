494165afad416be4de653cdcc8b5df46f73514f1
orucmu <mustafaoruc720@gmail.com>
2023-02-02 14:24:49 +0300

    Uygulama: Effect Hooks - Local Storage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { useState, useEffect } from "react";
+import NoteList from './components/NoteList'
 
 function App(props) {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState(props.text);
-
-  useEffect(() => {
-    console.log("count");
-  }, [count])
-
-  useEffect(() => {
-    console.log('componentDidMount');
-  }, [])
-
-  useEffect(() => {
-    console.log('componentDidUpdate');
-  });
-
   return (
-    <>
-      <p>Butona {count} kez tıkladınız.</p>
-      <p>Girilen text: {text}</p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(props.count)}>Reset</button>
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-    </>
-  );
-}
-
-App.defaultProps = {
-  count: 5, text: ''
+    <NoteList />
+  )
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App(props) {
     const [count, setCount] = useState(props.count);
-    const [text, setText] = useState(props.text)
+    const [text, setText] = useState(props.text);
+
+    useEffect(() => {
+        console.log("count");
+    }, [count])
+
+    useEffect(() => {
+        console.log('componentDidMount');
+    }, [])
+
+    useEffect(() => {
+        console.log('componentDidUpdate');
+    });
+
     return (
         <>
             <p>Butona {count} kez tıkladınız.</p>
@@ -13,6 +26,10 @@ function App(props) {
             <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         </>
     );
+}
+
+App.defaultProps = {
+    count: 5, text: ''
 }
 
 export default App;
